1c58f309ea53f9e6a26eac8a7e1e3913a4363f3d
Rich Hewitt <rich.e.hewitt@gmail.com>
2024-09-08 14:03:47 +0100

    pub 43

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A Nix-flake-based web environment with m4 preprocessor.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
   };
 
   outputs = { self , nixpkgs ,... }:
